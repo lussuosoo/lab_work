68355151a5cc3d524187d57c66cfbec039cdcd0b
lussuosoo <kristibedrina@gmail.com>
2025-02-21 12:14:27 +0300

    Initial commit: CMake project setup

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "utils/utils.h"
+
+int main() {
+    int a,b;
+    std::cin>>a>>b;
+    std::cout << "Sum: " << add(a,b) << std::endl;
+    std::cout << "Subtract: " << subtract(a,b) << std::endl;
+    return 0;
+}
